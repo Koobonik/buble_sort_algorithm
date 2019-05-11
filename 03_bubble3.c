9ba28b761433002b322ed76be827152a2e00ec5a
Koobonik <kbi960130@naver.com>
2019-05-11 14:48:06 +0900

    다른 소스코드에도 적용

--- 03_bubble3.c
+++ 03_bubble3.c
@@ -2,6 +2,8 @@
 
 #include <stdio.h> 
 #include <stdlib.h> 
+#include <time.h>
+
 
 #define swap(type, x, y) do {type t = x; x = y; y = t;} while (0)
 
@@ -24,17 +26,26 @@ void bubble(int a[], int n)
 
 int main(void)
 {
+	clock_t start, finish;
+	double duration;
+	start = clock();
+	int c[10] = {3, 5, 7, 11, 13, 17, 19, 23, 31, 37};
+	int j;
+	for (j = 0; j < 10; j++){
+		srand(c[j]);
 	int i, nx;
 	int * x; /* 배열의 첫 번째 요소에 대한 포인터 */
-
+	
 	puts("단순 교환 정렬");
 	printf("요솟수 :");
-	scanf("%d", &nx);
+	nx = 10000;
+	// scanf("%d", &nx);
 	x = calloc(nx, sizeof(int)); /* 요솟수 nx인 int 형 배열을 생성 */
 
 	for (i = 0; i < nx; i++) {
 		printf("x[%d] :", i);
-		scanf("%d", &x[i]);
+		x[i] = rand();
+		// scanf("%d", &x[i]);
 	}
 
 	bubble(x, nx); /* 배열 x를 단순 교환 정렬 */
@@ -44,6 +55,12 @@ int main(void)
 		printf("x[%d] = %d\n", i, x[i]);
 
 	free(x); /* 배열을 삭제 */
+	}
+	
+
+	finish = clock();
+	duration = (double) (finish - start) / CLOCKS_PER_SEC;
+	printf("%f 초입니다. \n", duration); 
 
 	return 0;
 }
