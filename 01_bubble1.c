65c74a9ca9d8cb0d2d6f161c33a0078a92b70377
Koobonik <kbi960130@naver.com>
2019-05-11 14:42:10 +0900

    시간 구하는 코드와 소수들 차례대로 계산

--- 01_bubble1.c
+++ 01_bubble1.c
@@ -1,6 +1,8 @@
 ﻿/* 버블 정렬 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
 
 #define swap(type, x, y) do { type t = x; x = y; y = t; } while (0)
 
@@ -17,25 +19,41 @@ void bubble(int a[], int n)
 
 int main(void)
 {
-	int i, nx;
-	int *x;							/* 배열의 첫 번째 요소에 대한 포인터 */
-	puts("버블 정렬");
-	printf("요소의 개수 : ");
-	scanf("%d", &nx);
-	x = calloc(nx, sizeof(int));	/* 요소의 개수가 nx인 int형 배열을 생성 */
+	clock_t start, finish;
+	double duration;
+	start = clock();
+	int c[10] = {3, 5, 7, 11, 13, 17, 19, 23, 31, 37};
+	int j;
+	for (j = 0; j < 10; j++){
+		srand(c[j]);
+		int i, nx;
+		int *x;							/* 배열의 첫 번째 요소에 대한 포인터 */
+		puts("버블 정렬");
+		printf("요소의 개수 : ");
+		//scanf("%d", &nx);
+		nx = 10000;
+		x = calloc(nx, sizeof(int));	/* 요소의 개수가 nx인 int형 배열을 생성 */
 
-	for (i = 0; i < nx; i++) {
-		printf("x[%d] : ", i);
-		scanf("%d", &x[i]);
+		for (i = 0; i < nx; i++) {
+			printf("x[%d] : ", i);
+			x[i] = rand();
+			// scanf("%d", &x[i]);
+		}
+
+		bubble(x, nx);		/* 배열 x를 버블 정렬 */
+
+		puts("오름차순으로 정렬했습니다.");
+		for (i = 0; i < nx; i++)
+			printf("x[%d] = %d\n", i, x[i]);
+	
+		free(x);			/* 배열 해제 */
 	}
+	
+	
+	finish = clock();
+	duration = (double) (finish - start) / CLOCKS_PER_SEC;
+	printf("%f 초입니다. \n", duration); 
 
-	bubble(x, nx);		/* 배열 x를 버블 정렬 */
-
-	puts("오름차순으로 정렬했습니다.");
-	for (i = 0; i < nx; i++)
-		printf("x[%d] = %d\n", i, x[i]);
-
-	free(x);			/* 배열 해제 */
 	
 	return 0;
 }
